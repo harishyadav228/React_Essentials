18d930d3d453444e70d20b826747011ff84bcaf2
harishyadav228 <154680600+harishyadav228@users.noreply.github.com>
2024-04-10 20:01:23 +0530

    first commit

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,15 @@
+
+
+
+export default function Tabs({children, buttons, ButtonsContainer ='menu'}){
+
+
+//const ButtonsContainer = buttonsContainer;
+
+    return(
+     <>
+        <ButtonsContainer>{buttons}</ButtonsContainer>
+             {children}
+     </>
+    );
+}
